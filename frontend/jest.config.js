3baca3f9085090a6a0cfdc9bbdcbf31f7fe8f6c2
The Kiet Dang <The.Kiet.Dang@signal-iduna.de>
2026-03-02 16:45:29 +0100

    add error

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,13 +1,14 @@
+const { createCjsPreset }  = require('jest-preset-angular/presets');
+
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'jest-preset-angular',
+  ...createCjsPreset,
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testMatch: ['**/+(*.)+(spec).+(ts)'],
   transform: {
     '^.+\\.(ts|mjs|js|html)$': ['ts-jest', {
       tsconfig: '<rootDir>/tsconfig.spec.json',
-      useESM: false,
-      isolatedModules: true
+      useESM: false,      
     }]
   },
   transformIgnorePatterns: ['node_modules/(?!(@angular|angular-oauth2-oidc|.*\\.mjs$))'],
@@ -16,6 +17,7 @@ module.exports = {
   "\\.(css|less|sass|scss)$": "identity-obj-proxy"
   },
   coverageDirectory: 'coverage',
+  collectCoverage: false, /* we collect through command line with jest --coverage */
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/app/app.routes.ts',
